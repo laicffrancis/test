76db96d945efb9230f18e92018661e0dabb21b86
laicffrancis <laicf@slcss.edu.hk>
2025-04-11 08:09:53 +0800

    Update server.js

--- nowtv-backend/server.js
+++ nowtv-backend/server.js
@@ -5,7 +5,8 @@ const app = express();
 app.get("/get-nowtv-url", (req, res) => {
     // 假设动态生成 Token
     const newToken = "new-generated-token"; // 替换为动态生成逻辑
-    const newUrl = `https://ewcdn15.nowe.com/session/10-${newToken}/Content/DASH_VOS3/Live/channel(VOS_CH332N)/manifest.mpd?token=${newToken}_1744277561`;
+    https://ewcdn102.nowe.com/session/10-ce05c1f086a249827d6131579cf3a/Content/DASH_VOS3/Live/channel(VOS_CH332N)/manifest.mpd?token=1eb4cbe0abfd0e6471a49bc4f76096f9_1744344498
+    const newUrl = `https://ewcdn102.nowe.com/session/10-${newToken}/Content/DASH_VOS3/Live/channel(VOS_CH332N)/manifest.mpd?token=${newToken}_1744344498`;
 
     res.json({ url: newUrl });
 });
